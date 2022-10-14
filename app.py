2670d7d0dd686109c9361ec01ebe5ad622d39d15
juanmarquesdev <juanbatistamarques@gmail.com>
2022-10-14 12:00:48 -0300

    adding migrations and db

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import email
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import Length, Email
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "THIS IS A SECRECT KEY"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
 Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    email = db.Column(db.String(128), primary_key=True)
+    password = db.Column(db.String(128))
 
 
 class LoginForm(FlaskForm):
@@ -44,10 +57,6 @@ def register():
         return "It's valid"
 
     return render_template("register.html", form=form)
-
-
-
-
 
 
 if __name__ == "__main__":
